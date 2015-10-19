80fec115c877be5320ab2077f7edf2293ab17557
李方舟 <lifangzhou@meituan.com>
2015-10-19 15:14:51 +0800

    update: no need to convert arguments to real array

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function combineCallbacks(arg0) {
   var callbacks = Array.isArray(arg0) ? arg0 : toArray(arguments)
 
   return function combination() {
-    var args = toArray(arguments)
+    var args = arguments
     var ctx = this
 
     callbacks.forEach(function(cb) {
