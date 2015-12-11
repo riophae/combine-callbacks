8c6ba83b82fc6603a57ba6ebdb75228bbaa16da3
李方舟 <lifangzhou@meituan.com>
2015-12-11 17:21:25 +0800

    remove useless vars

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ function combineCallbacks(arg0) {
   var callbacks = Array.isArray(arg0) ? arg0 : arguments
 
   return function combination() {
-    var args = arguments
-    var ctx = this
-
     for (var i = 0, len = callbacks.length; i < len; i++) {
-      callbacks[i].apply(ctx, args)
+      callbacks[i].apply(this, arguments)
     }
   }
 }
