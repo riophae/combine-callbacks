d35b3ab1abb84a277be971f5daa7e9de01d080d0
李方舟 <lifangzhou@meituan.com>
2015-12-18 12:34:31 +0800

    add more tests of advanced usage

--- test/index.js
+++ test/index.js
@@ -54,3 +54,37 @@ test('with context', function(t) {
   var combination = combineCallbacks(getCtx(done(t, [ arg, ctx ])))
   combination.call(ctx, arg)
 })
+
+test('should run in order of passed functions', function(t) {
+  var expected = [ 1, 2, 3, 4, 5 ]
+  var funcs = []
+  var arr = []
+
+  expected.forEach(function(n) {
+    funcs.push(function() {
+      arr.push(n)
+    })
+  })
+
+  var combination = combineCallbacks(funcs)
+  combination()
+
+  t.deepEqual(arr, expected)
+  t.end()
+})
+
+test('nested usage', function(t) {
+  var expected = [ 1, 2, 3, 4, 5 ]
+  var arr = []
+  var noop = function noop() {}
+
+  var combination = expected.reduce(function(f, n) {
+    return combineCallbacks(f, function() {
+      arr.push(n)
+    })
+  }, noop)
+
+  combination()
+  t.deepEqual(arr, expected)
+  t.end()
+})
